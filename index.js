aff0e8409598201adda73552b8bda1c79cf6a803
Remy Sharp <remy@remysharp.com>
2019-02-11 12:07:04 +0100

    feat: working proxy API
    Also blocks all *.cern.ch unless included in the whitelist file.

--- index.js
+++ index.js
@@ -1,45 +1,12 @@
 const request = require('request');
 const express = require('express');
-cont { parse } = require('url');
-const server = express();
+const bodyParser = require('body-parser');
+const app = express();
 
-const app = server.listen(process.env.PORT, details => {
-  console.log(`listening http://localhost:${app.address.port}`);
+app.use(bodyParser.urlencoded({ extended: false, limit: '50kb' }));
+
+app.use('/api', require('./lib/api'));
+
+const server = app.listen(process.env.PORT || 3000, details => {
+  console.log(`listening http://localhost:${server.address().port}`);
 });
-
-function inject(body, base) {
-  if (body.toUpperCase().indexOf('<BODY') === -1) {
-    // damn bodyless things...
-    body = '<body>' + body + '</body>';
-  }
-
-  $ = cheerio.load(body);
-  $ = prepare($, base ? '/proxy?url=' + base : '', require('url'));
-
-  return $.html();
-}
-
-function proxy(req, res, next) {
-  var url = parse(req.url, true);
-
-  if (url.pathname === '/www/proxy') {
-    var url = url.query.url;
-
-    if (!url) {
-      return next();
-    }
-    var base = parse(url).protocol + '//' + parse(url).hostname;
-
-    request(url, (error, response, body) => {
-      base = response.request.uri.href;
-      res.writeHead(200, { 'content-type': 'text/html' });
-      if (!error && response.statusCode == 200) {
-        return res.send(inject(body.replace(/&/g, '&amp;'), base));
-      }
-
-      res.send(404);
-    });
-  } else {
-    next();
-  }
-}
---
+++ whitelist
@@ -0,0 +1,1 @@
+info.cern.ch
