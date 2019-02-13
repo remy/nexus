98f89fcd217589b381bc3b59248783773315187e
Remy Sharp <remy@remysharp.com>
2019-02-13 15:08:37 +0100

    tidy build

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 require('@remy/envy');
-const request = require('request');
 const express = require('express');
 const app = express();
 
 app.use('/api', require('./lib/api'));
+app.use('/', express.static('./app'));
 
-const server = app.listen(process.env.PORT || 3000, details => {
+const server = app.listen(process.env.PORT || 3000, () => {
   console.log(`listening http://localhost:${server.address().port}`);
 });
