49ee3a562d68139a71b4705779bc361c12b5a524
Remy Sharp <remy@remysharp.com>
2019-02-13 10:21:20 +0100

    wip

--- src/components/WebView.js
+++ src/components/WebView.js
@@ -21,9 +21,12 @@ const WebView = ({ url, active = true, index, onNavigate, onClose }) => {
       body: JSON.stringify({ url }),
     });
     const json = await res.json();
-    if (res.status === 200) {
+    if (json.title) {
       setTitle(json.title);
       setBody(json.body);
+    } else {
+      // it went wrong…
+      console.log(json, res.status);
     }
   };
 
@@ -45,40 +48,42 @@ const WebView = ({ url, active = true, index, onNavigate, onClose }) => {
 
   return (
     <Draggable handle=".controls h2">
-      <div className="WebView" style={style}>
-        <div className={classnames(['title controls', { active }])}>
-          <button>
-            <img src="/assets/img/full-window-button.png" />
+      <div className="webview" style={style}>
+        <div className={classnames(['controls', { active }])}>
+          <button className="icon-buttons">
+            <img src="/assets/img/full-window-button.png" alt="Full Window" />
           </button>
           <h2>{title}</h2>
-          <button onClick={onClose}>
-            <img src="/assets/img/expand-window-button.png" />
+          <button className="icon-buttons" onClick={onClose}>
+            <img src="/assets/img/close-window-button.png" alt="Expand" />
           </button>
         </div>
-        <div
-          className="content"
-          contentEditable={true}
-          dangerouslySetInnerHTML={{ __html: body }}
-          spellCheck={false}
-          onClick={e => e.preventDefault()}
-          onDoubleClick={e => {
-            e.preventDefault();
-            if (e.target.nodeName === 'A') {
-              let navigateTo = e.target.href;
+        <div className="r2l-content">
+          <div
+            className="l2r-content content"
+            contentEditable={true}
+            dangerouslySetInnerHTML={{ __html: body }}
+            spellCheck={false}
+            onClick={e => e.preventDefault()}
+            onDoubleClick={e => {
+              e.preventDefault();
+              if (e.target.nodeName === 'A' && e.target.href) {
+                let navigateTo = e.target.href;
 
-              console.log(url, e.target.pathname);
+                console.log(url, e.target.pathname);
 
-              // if we're a relative url, then rebase since we're hosting the html
-              if (e.target.origin === HOST) {
-                navigateTo = new URL(
-                  e.target.getAttribute('href'),
-                  url
-                ).toString();
+                // if we're a relative url, then rebase since we're hosting the html
+                if (e.target.origin === HOST) {
+                  navigateTo = new URL(
+                    e.target.getAttribute('href'),
+                    url
+                  ).toString();
+                }
+                onNavigate(navigateTo);
               }
-              onNavigate(navigateTo);
-            }
-          }}
-        />
+            }}
+          />
+        </div>
       </div>
     </Draggable>
   );
