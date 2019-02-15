cd1c14bea0ae6bc2225a9977d795644cd7998e81
Angela Ricci <an.ricci@cern.ch>
2019-02-15 17:36:21 +0100

    config fix

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,11 @@
 module.exports = function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy('content/images');
-  eleventyConfig.addPassthroughCopy('content/css');
-  eleventyConfig.addPassthroughCopy('src/assets/fonts');
-
-  return {
-    passthroughFileCopy: true,
-    dir: {
-      input: 'content/',
-      output: 'build/www',
-    },
-  };
+ eleventyConfig.addPassthroughCopy(__dirname + ‘/content/images’);
+ eleventyConfig.addPassthroughCopy(__dirname + ‘/content/css’);
+ eleventyConfig.addPassthroughCopy(__dirname + ‘/src/assets/fonts’);  return {
+   passthroughFileCopy: true,
+   dir: {
+     input: __dirname + ‘/content/‘,
+     output: __dirname + ‘/build/www’,
+   },
+ };
 };
