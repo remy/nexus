6f4592a818551dc4f68a775b365c3bdc6e3c088b
Jeremy Keith <jeremy@adactio.com>
2019-02-15 17:55:35 +0100

    Fix config file for Eleventy

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,11 @@
 module.exports = function(eleventyConfig) {
- eleventyConfig.addPassthroughCopy(__dirname + ‘/content/images’);
- eleventyConfig.addPassthroughCopy(__dirname + ‘/content/css’);
- eleventyConfig.addPassthroughCopy(__dirname + ‘/src/assets/fonts’);  return {
+ eleventyConfig.addPassthroughCopy(__dirname + '/content/images');
+ eleventyConfig.addPassthroughCopy(__dirname + '/content/css');
+ eleventyConfig.addPassthroughCopy(__dirname + '/src/assets/fonts');  return {
    passthroughFileCopy: true,
    dir: {
-     input: __dirname + ‘/content/‘,
-     output: __dirname + ‘/build/www’,
+     input: __dirname + '/content/',
+     output: __dirname + '/build/www',
    },
  };
 };
