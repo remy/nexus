aa9d97e68bbda84e20e79ee1bfff1f489adad03c
Remy Sharp <remy@remysharp.com>
2019-02-15 23:37:18 +0100

    feat: support #target

--- TODO.md
+++ TODO.md
@@ -3,3 +3,8 @@
 - [ ] Highlight menus that are fired via keyboard
 - [x] Don't let open menu re-open
 - [ ] If menu is moved, add close handler
+
+ampersand (&AMP;)
+less than (&LT;)
+greater than (&GT;)
+quote (&QUOT;)
--- index.js
+++ index.js
@@ -12,6 +12,10 @@ app.use('/api', require('./lib/api'));
 app.use('/browser', express.static('./build/browser'));
 app.use('/', express.static('./build/www'));
 
+if (process.env.NODE_ENV === 'development') {
+  app.use('/www/', express.static('./src/assets/www'));
+}
+
 const server = app.listen(process.env.PORT || 3000, () => {
   console.log(`listening http://localhost:${server.address().port}`);
 });
