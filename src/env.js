d7a7c0862bbda559e9c7d7eba6c6b35031c50a4e
Remy Sharp <remy@remysharp.com>
2019-02-16 12:49:13 +0000

    feat: add meta in the log to help diagnose

--- src/env.js
+++ src/env.js
@@ -7,4 +7,13 @@ if (API.endsWith('/')) API = API.slice(0, -1);
 
 const PATH = HOST + '/browser';
 
+const SHA = process.env.npm_package_gitHead;
+console.groupCollapsed('version', SHA.substr(0, 8));
+console.log('SHA %s', SHA);
+console.log('HOST %s', HOST);
+console.log('API %s', API);
+console.log(`https://gitlab.cern.ch/nexus-project/nexus-browser/commit/${SHA}`);
+
+console.groupEnd('version');
+
 export { API, HOST, PATH };
