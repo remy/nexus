c433cf6c702077527d311e214cb4b3d54892b8d2
Remy Sharp <remy@remysharp.com>
2019-02-21 20:19:46 +0000

    fix: +robots.txt

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,14 @@
 module.exports = function(eleventyConfig) {
- eleventyConfig.addPassthroughCopy(__dirname + '/content/images');
- eleventyConfig.addPassthroughCopy(__dirname + '/content/css');
- eleventyConfig.addPassthroughCopy(__dirname + '/src/assets/fonts');  return {
-   passthroughFileCopy: true,
-   dir: {
-     input: __dirname + '/content/',
-     output: __dirname + '/build/www',
-   },
- };
+  eleventyConfig.addPassthroughCopy(__dirname + '/content/images');
+  eleventyConfig.addPassthroughCopy(__dirname + '/content/robots.txt');
+  eleventyConfig.addPassthroughCopy(__dirname + '/content/css');
+  eleventyConfig.addPassthroughCopy(__dirname + '/src/assets/fonts');
+
+  return {
+    passthroughFileCopy: true,
+    dir: {
+      input: __dirname + '/content/',
+      output: __dirname + '/build/www',
+    },
+  };
 };
