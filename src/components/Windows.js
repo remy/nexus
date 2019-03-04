2efd85bf98cfff188273ef06c54268e95cc3ab5a
Remy Sharp <remy@remysharp.com>
2019-03-04 11:51:51 +0000

    chore: tiny refactor

--- src/components/Windows.js
+++ src/components/Windows.js
@@ -4,15 +4,12 @@ const Windows = ({ windows, add, actionHandler, setActive, active }) =>
   windows.map(({ Component, id, ...props }, index) => {
     return (
       <Component
-        key={`panel:${id}`}
-        id={id}
-        index={index}
-        add={add}
-        actionHandler={actionHandler}
-        close={close('panel')}
         {...props}
+        {...{ id, index, add, actionHandler }}
         active={active.id == id}
         onAction={url => add({ type: 'url', id: url })}
+        key={`panel:${id}`}
+        close={close('panel')}
         onFocus={() => setActive({ type: 'panel', id })}
         onClose={close('panel')}
       />
